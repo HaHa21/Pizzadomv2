1388a50789e840e64ef57e65abc933af1f665778
HaHa21 <domlee2012@gmail.com>
2018-01-23 17:41:17 +0000

    Add files via upload
    object-oriented code modification recent

--- app/src/main/java/com/pizzadom/pizzadom/Forgotpassword.java
+++ app/src/main/java/com/pizzadom/pizzadom/Forgotpassword.java
@@ -27,7 +27,7 @@ import static android.R.attr.mode;
  * Created by User on 24/6/2017.
  */
 
-public class Forgotpassword extends AppCompatActivity implements View.OnClickListener {
+public class Forgotpassword extends AppCompatActivity {
     private TextView txtWelcome;
     private EditText input_new_password;
     private Button btnChangePass, btnLogout, btnback;
@@ -47,8 +47,8 @@ public class Forgotpassword extends AppCompatActivity implements View.OnClickLis
         btnback = (Button) findViewById(R.id.btnback);
 
 
-        btnChangePass.setOnClickListener(this);
-        btnback.setOnClickListener(this);
+        btnChangePass.setOnClickListener(new ButtonHandlerForgotPassword(this, 1, PD, input_new_password, auth));
+        btnback.setOnClickListener(new ButtonHandlerForgotPassword(this, 2, PD, input_new_password, auth));
         PD = new ProgressDialog(this);
         PD.setMessage("Loading...");
         PD.setCancelable(true);
@@ -63,6 +63,7 @@ public class Forgotpassword extends AppCompatActivity implements View.OnClickLis
 
     }
 
+    /*
     @Override
     public void onClick(View v) {
        if(v.getId() == R.id.dashboard_btn_change_pass){
@@ -95,11 +96,56 @@ public class Forgotpassword extends AppCompatActivity implements View.OnClickLis
            startActivity(i);
        }
     }
+    */
 
+}
 
+class ButtonHandlerForgotPassword implements View.OnClickListener {
+    public Forgotpassword btnhandler;
+    public int statehandler;
+    private EditText input_new_password;
+    private FirebaseAuth auth;
+    private ProgressDialog PD;
 
-
+    public ButtonHandlerForgotPassword(Forgotpassword btnhandler, int statehandler, ProgressDialog PD, EditText input_new_password, FirebaseAuth auth){
+        this.btnhandler = btnhandler;
+        this.statehandler = statehandler;
+        this.PD = PD;
+        this.input_new_password = input_new_password;
+        this.auth = auth;
     }
+
+    @Override
+    public void onClick(View v) {
+        if(statehandler == 1) {
+            String email_id = input_new_password.getText().toString().trim();
+            auth.sendPasswordResetEmail(email_id)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                PD.show();
+                                PD.dismiss();
+                                Toast.makeText(btnhandler, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+
+
+                            } else {
+                                PD.show();
+                                PD.dismiss();
+                                Toast.makeText(btnhandler, "Failed to send reset email !! This Email is not registerd !!", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }
+                    });
+        }
+
+        else if(statehandler == 2){
+            Intent i = new Intent(btnhandler, LoginActivity.class);
+            btnhandler.startActivity(i);
+        }
+    }
+}
 
 
 
--- app/src/main/java/com/pizzadom/pizzadom/LoginActivity.java
+++ app/src/main/java/com/pizzadom/pizzadom/LoginActivity.java
@@ -2,6 +2,7 @@ package com.pizzadom.pizzadom;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -39,163 +41,83 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+
+import butterknife.BindView;
+
+import butterknife.ButterKnife;
+
 
 
 /**
  * Created by User on 24/6/2017.
  */
 
-public class LoginActivity extends AppCompatActivity implements OnClickListener {
+public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "Pizzadom";
-    Button btnLogin,btnSignUp, btnForgotPass;
-    ImageButton btnback, btnforward;
-    EditText input_email, input_password;
-    private String password, email;
+    @BindView(R.id.Email)
+    EditText input_email;
+    @BindView(R.id.Password)
+    EditText input_password;
+
+    @BindView(R.id.login)
+    Button btnlogin;
+    @BindView(R.id.ForgotPass)
+    Button forgotpass;
+    @BindView(R.id.SignUp)
+    Button signup;
+    @BindView(R.id.backbtn)
+    ImageButton btnback;
+    @BindView(R.id.forwardbtn)
+    ImageButton btnforward;
+
+    public String email, password;
+
     private FirebaseAuth.AuthStateListener mAuthListener;
     private Firebase mRef;
     private FirebaseAuth auth;
-    private CoordinatorLayout login;
+    @BindView(R.id.loginlayout)
+    CoordinatorLayout login;
     //progress dialog
     private ProgressDialog progressDialog;
     //FaceBook callbackManager
     private CallbackManager callbackManager;
-    //
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.login);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
 
-        btnLogin = (Button) findViewById(R.id.login);
-        btnForgotPass = (Button) findViewById(R.id.ForgotPass);
-        btnSignUp = (Button) findViewById(R.id.SignUp);
-        btnback = (ImageButton) findViewById(R.id.backbtn);
-        btnforward = (ImageButton) findViewById(R.id.forwardbtn);
-        input_email = (EditText) findViewById(R.id.Email);
-        input_password = (EditText) findViewById(R.id.Password);
-        login = (CoordinatorLayout) findViewById(R.id.loginlayout);
+        ButterKnife.bind(this);
 
-        // Find the toolbar view inside the activity layout
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Pizzadom");
 
         setSupportActionBar(toolbar);
 
+
+
+        /*
         if(savedInstanceState != null){
             password = savedInstanceState.getString("password");
             email = savedInstanceState.getString("email");
-        }
-
-        progressDialog = new ProgressDialog(this);
-        btnSignUp.setOnClickListener(this);
-        btnForgotPass.setOnClickListener(this);
-        btnLogin.setOnClickListener(this);
-        btnback.setOnClickListener(this);
-        btnforward.setOnClickListener(this);
-        //Add YOUR Firebase Reference URL instead of the following URL
-         mRef = new Firebase("https://pizzadom-3caec.firebaseio.com/users/");
-        //Get Firebase auth instance
+        }*/
         auth = FirebaseAuth.getInstance();
-        FirebaseUser mUser = auth.getCurrentUser();
-        if (mUser != null) {
-            // User is signed in
-            Intent intent = new Intent(getApplicationContext(), WelcomeShoppingActivity.class);
-            String uid = auth.getCurrentUser().getUid();
-            String image = auth.getCurrentUser().getPhotoUrl().toString();
-            intent.putExtra("user_id", uid);
-            if(image!=null || image!=""){
-                intent.putExtra("profile_picture",image);
-            }
-            startActivity(intent);
-            finish();
-            Log.d(TAG, "onAuthStateChanged:signed_in:" + mUser.getUid());
-        }
+        progressDialog = new ProgressDialog(this);
 
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser mUser = firebaseAuth.getCurrentUser();
-                if (mUser != null) {
-                    // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + mUser.getUid());
-                } else {
-                    // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
 
-            }
-        };
+        btnlogin.setOnClickListener(new ButtonHandler(1, input_password, input_email, this, auth, progressDialog));
+        forgotpass.setOnClickListener(new ButtonHandler(2, input_password, input_email, this, auth, progressDialog));
+        signup.setOnClickListener(new ButtonHandler(3, input_password, input_email, this, auth, progressDialog));
+        btnback.setOnClickListener(new ButtonHandler(4, input_password, input_email, this, auth, progressDialog));
+        btnforward.setOnClickListener(new ButtonHandler(5, input_password, input_email, this, auth, progressDialog));
 
-        //FaceBook
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton) findViewById(R.id.button_facebook_login);
-        loginButton.setReadPermissions("email", "public_profile");
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Log.d(TAG, "facebook:onSuccess:" + loginResult);
-                signInWithFacebook(loginResult.getAccessToken());
-            }
-
-            @Override
-            public void onCancel() {
-                Log.d(TAG, "facebook:onCancel");
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                Log.d(TAG, "facebook:onError", error);
-            }
-        });
     }
-
-    public void onSavedInstanceState(Bundle saveState){
-        saveState.putString("email", input_email.getText().toString());
-        saveState.putString("password", input_password.getText().toString());
-    }
-
-    private void signInWithFacebook(AccessToken accessToken) {
-        Log.d(TAG, "signInWithFacebook:" + accessToken);
-
-        showProgressDialog();
-
-
-        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
-        auth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithCredential", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }else{
-                            String uid=task.getResult().getUser().getUid();
-                            String name=task.getResult().getUser().getDisplayName();
-                            String email=task.getResult().getUser().getEmail();
-                            String image=task.getResult().getUser().getPhotoUrl().toString();
-
-                            //Create a new User and Save it in Firebase database
-                            User user = new User(uid,name,null,email,null);
-
-                            mRef.child(uid).setValue(user);
-                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                            intent.putExtra("user_id",uid);
-                            intent.putExtra("profile_picture",image);
-                            startActivity(intent);
-                            finish();
-                        }
-
-                        hideProgressDialog();
-                    }
-                });
-                        }
 
     private void showProgressDialog() {
         if (progressDialog == null) {
@@ -259,73 +181,154 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
         }
     }
 
-    @Override
-    public void onClick(View v) {
-        String email = input_email.getText().toString();
-        final String password = input_password.getText().toString();
-
-       if(v.getId() == R.id.ForgotPass) {
-           startActivity(new Intent(LoginActivity.this, Forgotpassword.class));
-           finish();
-       }
-        else if(v.getId() == R.id.SignUp) {
-           startActivity(new Intent(LoginActivity.this, Firebaseregister.class));
-           finish();
-       }
-       else if(v.getId() == R.id.login){
-
-           if(TextUtils.isEmpty(password)){
-               Toast.makeText(this, "Please enter password ", Toast.LENGTH_LONG).show();
-               return;
-           }
-
-           if(TextUtils.isEmpty(email)){
-               Toast.makeText(this, "Please enter email ", Toast.LENGTH_LONG).show();
-               return;
-           }
 
 
-           auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>(){
+}
 
-                       @Override
-                       public void onComplete(@NonNull Task<AuthResult> task) {
+class ButtonHandler implements OnClickListener{
+    private static final String TAG = "Pizzadom";
+    ButtonHandler btn;
+    int statehandler;
+
+    EditText password;
+    EditText email;
+    LoginActivity login;
+    FirebaseAuth auth;
+    CallbackManager callbackManager;
+    //progress dialog
+    ProgressDialog progressDialog;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private Firebase mRef;
+    String input_pass;
+    String input_email;
 
 
-                           if(!task.isSuccessful()){
+    public ButtonHandler(int statehandler, EditText password, EditText email,
+                         LoginActivity login, FirebaseAuth auth, ProgressDialog progressDialog){
+        this.statehandler = statehandler;
+        this.password = password;
+        this.email = email;
+        this.login = login;
+        this.auth = auth;
+        this.progressDialog = progressDialog;
 
-
-                               Log.w(TAG, "signInWithEmail", task.getException());
-                               Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                       Toast.LENGTH_SHORT).show();
-                           }
-                           else {
-                               progressDialog.setMessage("Logging in Please Wait...");
-                               progressDialog.setCancelable(true);
-                               progressDialog.setCanceledOnTouchOutside(false);
-                               progressDialog.show();
-                               progressDialog.dismiss();
-                               Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                               String uid = auth.getCurrentUser().getUid();
-                               intent.putExtra("user_id", uid);
-                               startActivity(intent);
-                               finish();
-                           }
-                          hideProgressDialog();
-                       }
-                   }
-           );
-       }
-       else if(v.getId() == R.id.backbtn) {
-           Intent intenthome = new Intent(this, MainActivity.class);
-           startActivity(intenthome);
-           finish();
-       }
-       else if(v.getId() == R.id.forwardbtn){
-           Intent intentshopping = new Intent(this, ShoppingActivity.class);
-           startActivity(intentshopping);
-           finish();
-       }
     }
 
+    @Override
+    public void onClick(View v) {
+
+        input_pass = login.input_password.getText().toString();
+        input_email = login.input_email.getText().toString();
+
+        if(statehandler == 1){
+
+            if(TextUtils.isEmpty(input_email)){
+                Toast.makeText(login, "Please enter email ", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+             if(TextUtils.isEmpty(input_pass)){
+                Toast.makeText(login, "Please enter password ", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            signinTheUserWithEmailAndPassword(input_email, input_pass);
+
+        }else if(statehandler == 2){
+            login.startActivity(new Intent(login, Forgotpassword.class));
+            login.finish();
+        }else if(statehandler == 3){
+            login.startActivity(new Intent(login, Firebaseregister.class));
+            login.finish();
+        }else if(statehandler == 4){
+            login.startActivity(new Intent(login, MainActivity.class));
+            login.finish();
+        }else if(statehandler == 5){
+            Intent intentshop = new Intent(login, ShoppingActivity.class);
+            login.startActivity(intentshop);
+            login.finish();
+        }
+    }
+
+    private void signinTheUserWithEmailAndPassword(String userEmail, String userPassword){
+        auth = FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(
+                login, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                           Toast.makeText(login, "There was an error", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(login, "Login successful", Toast.LENGTH_SHORT).show();
+                            /*
+                            showProgressDialog();
+                           Intent welcomeIntent = new Intent(login, WelcomeShoppingActivity.class);
+                            login.startActivity(welcomeIntent);
+                            login.finish();
+                            hideProgressDialog();
+                            */
+                        }
+                    }
+                }
+        );
+    }
+
+    private void signInWithFacebook(AccessToken accessToken) {
+        Log.d(TAG, "signInWithFacebook:" + accessToken);
+
+        showProgressDialog();
+
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(login, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(login, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            String uid=task.getResult().getUser().getUid();
+                            String name=task.getResult().getUser().getDisplayName();
+                            String email=task.getResult().getUser().getEmail();
+                            String image=task.getResult().getUser().getPhotoUrl().toString();
+
+                            //Create a new User and Save it in Firebase database
+                            User user = new User(uid,name,null,email,null);
+
+                            mRef.child(uid).setValue(user);
+                            Intent intent = new Intent(login, MainActivity.class);
+                            intent.putExtra("user_id",uid);
+                            intent.putExtra("profile_picture",image);
+                            login.startActivity(intent);
+                            login.finish();
+                        }
+
+                        hideProgressDialog();
+                    }
+                });
+    }
+
+    private void showProgressDialog() {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(login);
+            progressDialog.setMessage(login.getString(R.string.loading));
+            progressDialog.setIndeterminate(true);
+        }
+
+        progressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+    }
 
 }
