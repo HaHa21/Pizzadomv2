df759fb36976a160aee96f09944955b453487ce8
HaHa21 <domlee2012@gmail.com>
2018-01-25 11:20:51 +0000

    Commit Main Activity using BindVIew annotation

--- app/src/main/java/com/pizzadom/pizzadom/MainActivity.java
+++ app/src/main/java/com/pizzadom/pizzadom/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
+
 import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.Toast;
