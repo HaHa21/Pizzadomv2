42b554765149290cdcad3b921f1d77d5cfd68fcd
HaHa21 <domlee2012@gmail.com>
2018-01-25 11:09:58 +0000

    Commit Main Activity using BindVIew annotation

--- app/src/main/java/com/pizzadom/pizzadom/MainActivity.java
+++ app/src/main/java/com/pizzadom/pizzadom/MainActivity.java
@@ -1,62 +1,68 @@
 package com.pizzadom.pizzadom;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
 import com.daimajia.slider.library.Indicators.PagerIndicator;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
-import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
 
 
 import java.util.HashMap;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity implements BaseSliderView.OnSliderClickListener,
         ViewPagerEx.OnPageChangeListener, OnClickListener {
     private android.support.v7.widget.ShareActionProvider shareActionProvider;
+    private PopupWindow pw;
 
-    private SliderLayout imageSlider;
+    @BindView(R.id.btnpopup)
+    public Button btnpopup;
+    @BindView(R.id.logintext)
+    public Button login;
+    @BindView(R.id.goshopping)
+    public Button gotoshopping;
+    @BindView(R.id.slider)
+    public SliderLayout imageSlider;
+    @BindView(R.id.toolbar)
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find the toolbar view inside the activity layout
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         toolbar.setTitle("Pizzadom");
         setSupportActionBar(toolbar);
 
 
-        Button login = (Button) findViewById(R.id.logintext);
 
-        imageSlider = (SliderLayout)findViewById(R.id.slider);
-
-        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
-          file_maps.put("Beef_Pepperoni_Pizza",R.drawable.photo1);
-          file_maps.put("Cheezy_Pizza",R.drawable.photo2);
-          file_maps.put("Vegetarian_Pizza",R.drawable.photo3);
+        HashMap<String, Integer> file_maps = new HashMap<>();
+        file_maps.put("Beef_Pepperoni_Pizza", R.drawable.photo1);
+        file_maps.put("Cheezy_Pizza", R.drawable.photo2);
+        file_maps.put("Vegetarian_Pizza", R.drawable.photo3);
 
 
-        for(String name : file_maps.keySet()){
+        for (String name : file_maps.keySet()) {
             TextSliderView textSliderView = new TextSliderView(this);
             // initialize a SliderLayout
             textSliderView
@@ -68,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
             //add your extra information
             textSliderView.bundle(new Bundle());
             textSliderView.getBundle()
-                    .putString("extra",name);
+                    .putString("extra", name);
 
             imageSlider.addSlider(textSliderView);
         }
@@ -79,34 +85,43 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
         imageSlider.setDuration(12000);
         imageSlider.addOnPageChangeListener(this);
         login.setOnClickListener(this);
+        btnpopup.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                LayoutInflater layoutInflater
+                        = (LayoutInflater)getBaseContext()
+                        .getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = layoutInflater.inflate(R.layout.ad_content, null);
+                final PopupWindow popupWindow = new PopupWindow(
+                        popupView,
+                        LayoutParams.WRAP_CONTENT,
+                       LayoutParams.WRAP_CONTENT);
+
+                Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
+                btnDismiss.setOnClickListener(new Button.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        // TODO Auto-generated method stub
+                        popupWindow.dismiss();
+                    }});
+
+                popupWindow.showAsDropDown(btnpopup, 50, 150);
+
+            }});
+
 
 
     }
 
-
-    /*public void login(View v) {
-
-            Intent intentlogin = new Intent(this , LoginActivity.class);
-            startActivity(intentlogin);
-
-    }*/
-
     @Override
-    protected void onStop() {
-        /* make sure to call stopAutoCycle() on the slider before activity
-        or fragment is destroyed*/
-        imageSlider.stopAutoCycle();
-        super.onStop();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
         return true;
     }
-
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
@@ -141,7 +156,20 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
             default: return super.onOptionsItemSelected(item);
         }
     }
+    /*public void login(View v) {
 
+            Intent intentlogin = new Intent(this , LoginActivity.class);
+            startActivity(intentlogin);
+
+    }*/
+
+    @Override
+    protected void onStop() {
+        /* make sure to call stopAutoCycle() on the slider before activity
+        or fragment is destroyed*/
+        imageSlider.stopAutoCycle();
+        super.onStop();
+    }
 
     @Override
     public void onSliderClick(BaseSliderView slider) {
@@ -163,6 +191,8 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
     public void onPageScrollStateChanged(int state) {
 
     }
+
+
 
 
     @Override
